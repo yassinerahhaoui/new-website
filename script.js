bba6cc2c2c9e9dfc7f93110c4b1f92d20d76a396
yassinerahhaoui <yassinerahhaoui6@gmail.com>
2019-12-01 11:52:25 +0100

    --version 1.02.6

---
+++ script.js
@@ -0,0 +1,15 @@
+function facebook() {
+    window.open("https://www.facebook.com", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=100,left=500,width=400,height=500");
+}
+function twitter() {
+    window.open("https://www.twitter.com", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=100,left=500,width=400,height=500");
+}
+function snapchat() {
+    window.open("https://www.snapchat.com", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=100,left=500,width=400,height=500");
+}
+function youtube() {
+    window.open("https://www.youtube.com", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=100,left=500,width=400,height=500");
+}
+function instagram() {
+    window.open("https://www.instagram.com", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=100,left=500,width=400,height=500");
+}
